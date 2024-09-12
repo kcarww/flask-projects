99c2073edd18c67291cfac8c4705d71b09d43c72
kcarww <carlos.oe.cc@gmail.com>
2024-09-12 20:03:20 -0300

    feat: consumindo api

--- aula01/main.py
+++ aula01/main.py
@@ -1,6 +1,7 @@
-# pylint: disable=E0401,C0116,C0114,C0103
+# pylint: disable=E0401,C0116,C0114,C0103,C0301,
+import urllib.request
+import json
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
 
 
@@ -27,14 +28,29 @@ def sobre():
     return render_template('sobre.html')
 
 
-
 frutas = []
-@app.route('/form', methods=['GET','POST'])
+
+
+@app.route('/form', methods=['GET', 'POST'])
 def cadastrarFruta():
     if request.method == 'POST':
         if request.form.get('fruta'):
             frutas.append(request.form.get('fruta'))
     return render_template('formulario.html', frutas=frutas)
+
+
+@app.route('/filmes')
+def filmes():
+    url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3ddc9b92db4de6c6559569c67bd88a13"
+
+    resposta = urllib.request.urlopen(url)
+
+    dados = resposta.read()
+
+    jsondata = json.loads(dados)
+
+    return render_template("filmes.html", filmes=jsondata['results'])
+    # return jsondata
 
 
 if __name__ == '__main__':
