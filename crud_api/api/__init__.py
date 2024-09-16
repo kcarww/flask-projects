83b2d26723c4e255a99e2b18dc10c9b1f7cf0893
kcarww <carlos.oe.cc@gmail.com>
2024-09-16 16:17:56 -0300

    feat: create migration

---
+++ crud_api/api/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+api = Api(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from .views import curso_views
+from .models import curso_model
